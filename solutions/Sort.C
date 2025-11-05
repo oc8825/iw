61a6e835a3ef5db8abb66a095a98a91235df2e7d
Owen Clarke <oc8825@princeton.edu>
2025-11-05 12:48:13 -0500

    running with new files structure

--- solutions/Sort.C
+++ solutions/Sort.C
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 
 int compare(const void *a, const void *b) {
-    int int_a = *((int*)a);
-    int int_b = *((int*)b);
-    
-    if (int_a == int_b) return 0;
-    else if (int_a < int_b) return -1;
-    else return 1;
+    int ia = *(const int*)a;
+    int ib = *(const int*)b;
+    return (ia > ib) - (ia < ib);
 }
 
 int main(int argc, char *argv[]) {
